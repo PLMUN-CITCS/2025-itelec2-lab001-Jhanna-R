60abbf425474bc1a594313a46a1f03e71fdca3b4
Janna Roldan <162396417+Jhanna-R@users.noreply.github.com>
2025-02-10 13:24:58 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Hello world!")
-print("Name: [Your Full Name]")
-print("Course: [Your Course Name]")
-print("Interests in programming: [Your Interests]")
+print("Name: Janna Roldan")
+print("Course: BSIT")
+print("Interests in programming: to learn more something about in coding.")
